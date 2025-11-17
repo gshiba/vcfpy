5d096f46fe118f727b8eabad4f9eccad899337d9
Gosuke Shibahara <goskays@gmail.com>
2025-11-17 23:45:15 +0000

    fix

--- src/vcfpy/reader.py
+++ src/vcfpy/reader.py
@@ -192,13 +192,10 @@ class Reader:
         """
         if self.tabix_iter:
             result = self.parser.parse_line(str(next(self.tabix_iter)))
-            if result is None:
-                raise StopIteration()
-            else:
-                return result
         else:
             result = self.parser.parse_next_record()
-            if result is None:
-                raise StopIteration()
-            else:
-                return result
+
+        if result is None:
+            raise StopIteration()
+
+        return result
